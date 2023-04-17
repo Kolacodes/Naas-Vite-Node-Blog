a2294e78c517c796dd6848d1a85c245086548aad
Kolacodes <afrocodes@gmail.com>
2023-04-17 12:27:02 +0100

    make better

--- api/client/src/components/post/Post.jsx
+++ api/client/src/components/post/Post.jsx
@@ -1,33 +1,26 @@
-import './post.css'
-import { Link } from 'react-router-dom'
+import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post({post}) {
+export default function Post({ post }) {
+  const PF = "https://naas-blog.onrender.com/images/";
   return (
-	<div className='post'>
-		{post.photo && (
-			<img src="https://images.unsplash.com/photo-1646624807990-18ad54729bbd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="" className="postImg"
-			/>
-		)}
-	  
-	  <div className="postInfo">
-		<div className="postCats">
-			{post.categories.map((c) => (
-			  <span className="postCat">{c.name}</span>
-			))}
-		</div>
-
-		<Link to={`/post/${post._id}`} className='link'>
-		<span className="postTitle">{ post.title}</span>
-		</Link>
-
-
-		
-		<hr />
-		<span className="postDate">{new Date(post.createdAt).toDateString()} </span>
-	  </div>
-	  <p className="postDesc">
-		{}
-	  </p>
-	</div>
-  )
+    <div className="post">
+      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
+        </div>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
+      </div>
+      <p className="postDesc">{post.desc}</p>
+    </div>
+  );
 }
