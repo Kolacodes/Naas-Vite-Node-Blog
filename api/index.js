d25fe0dfd979542cfceef5e2d653ec81dfe82f17
Kolacodes <afrocodes@gmail.com>
2023-04-19 13:30:59 +0100

    make better

--- api/index.js
+++ api/index.js
@@ -9,6 +9,8 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require('path');
 const cors = require('cors');
+const ejs = require('ejs');
+
 
 // Allow all origins to access the API
 // app.use(cors());
@@ -30,23 +32,111 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch(err => console.log(err));
 
 
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-	  cb(null, "images");
-	},
-	filename: (req, file, cb) => {
-	  cb(null, req.body.name);
-	},
-  });
-  
 
-  
-  const upload = multer({ storage: storage });
-  app.post("/api/upload", upload.single("file"), (req, res) => {
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
-	res.status(200).json("File has been uploaded");
-	// console.log("File has been uploaded");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: 'images',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('file');
+
+// Check File Type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+
+app.post('/api/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      console.log(err);
+    } else {
+      if(req.file == undefined){
+        res.json('no file man....')
+      } else {
+	   res.status(200).json("File has been uploaded");
+      }
+    }
   });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   MULTER MIDDLEWARE
+
+// const storage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 	  cb(null, "images");
+// 	},
+// 	filename: (req, file, cb) => {
+// 	  cb(null, req.body.name);
+// 	},
+//   });
+
+//   const upload = multer({ storage: storage });
+
+//   app.post("/api/upload", upload.single("file"), (req, res) => {
+// 	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+// 	res.status(200).json("File has been uploaded");
+//   });
+
+//  END OF MULTER MIDDLEWARE
+
 
 
 app.use('/api/auth', authRoute);
@@ -61,6 +151,6 @@ app.get("*", (req, res) => {
 })
 
 
-app.listen(process.env.PORT || '5000', () => {
+app.listen(5000, () => {
 	console.log("Backend is running...")
 })
